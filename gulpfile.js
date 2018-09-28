ac8a1e81c012617f14a492833e130fb302a15600
mtc-space <43601472+mtc-space@users.noreply.github.com>
2018-09-28 16:55:09 +0200

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 var gulp          = require('gulp');
 var sass          = require('gulp-sass');
-var browserSynch  = require('browser-synch');
+var browserSynch  = require('browser-sync');
 
 // Compilation du code .scss vers css
 gulp.task('sass', function() {
@@ -15,6 +15,10 @@ gulp.task('sass', function() {
 // Observation de modification de fichier .scss
 gulp.task('watch', ['browserSynch', 'sass'], function() {
   gulp.watch('assets/sass/**/*.scss', ['sass']);
+  // Rafraîchir la page lors des modifications HTML, PHP ou JS
+  gulp.watch('*.html', browserSynch.reload);
+  gulp.watch('*.js', browserSynch.reload);
+  gulp.watch('*.php', browserSynch.reload);
 });
 
 // Rafraîchissement auto des navigateurs web
