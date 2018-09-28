7698736f01096ea546665e7b49f068cdd71c33b1
mtc-space <43601472+mtc-space@users.noreply.github.com>
2018-09-28 16:40:28 +0200

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,59 +1,27 @@
-const gulp = require('gulp');
-const plumber = require('gulp-plumber');
-const sass = require('gulp-sass');
-const postcss = require('gulp-postcss');
-const autoprefixer = require('autoprefixer');
-const sourcemaps = require('gulp-sourcemaps');
-const groupmq = require('gulp-group-css-media-queries');
-const bs = require('browser-sync');
+var gulp          = require('gulp');
+var sass          = require('gulp-sass');
+var browserSynch  = require('browser-synch');
 
-const SASS_SOURCES = [
-  './*.scss', // This picks up our style.scss file at the root of the theme
-  'css/**/*.scss', // All other Sass files in the /css directory
-];
-
-/**
- * Compile Sass files
- */
-gulp.task('compile:sass', ['lint:sass'], () =>
-  gulp.src(SASS_SOURCES, { base: './' })
-    .pipe(plumber()) // Prevent termination on error
-    .pipe(sass({
-      indentType: 'tab',
-      indentWidth: 1,
-      outputStyle: 'expanded', // Expanded so that our CSS is readable
-    })).on('error', sass.logError)
-    .pipe(postcss([
-      autoprefixer({
-        browsers: ['last 2 versions'],
-        cascade: false,
-      })
-    ]))
-    .pipe(groupmq()) // Group media queries!
-    .pipe(gulp.dest('.')) // Output compiled files in the same dir as Sass sources
-    .pipe(bs.stream())); // Stream to browserSync
-
-/**
- * Start up browserSync and watch Sass files for changes 
- */
-gulp.task('watch:sass', ['compile:sass'], () => {
-  bs.init({
-    proxy: 'http://localhost/wordpress-installation'
-  });
-
-  gulp.watch(SASS_SOURCES, ['compile:sass', 'lint:sass']);
+// Compilation du code .scss vers css
+gulp.task('sass', function() {
+  return gulp.src('assets/sass/**/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('assets/css'))
+    .pipe(browserSynch.reload({
+      stream: true
+    }))
 });
 
-/**
- * Lint Sass
- */
-gulp.task('lint:sass', () =>
-  gulp.src(SASS_SOURCES)
-    .pipe(plumber())
-    .pipe(sassLint())
-    .pipe(sassLint.format()));
+// Observation de modification de fichier .scss
+gulp.task('watch', ['browserSynch', 'sass'], function() {
+  gulp.watch('assets/sass/**/*.scss', ['sass']);
+});
 
-/**
- * Default task executed by running `gulp`
- */
-gulp.task('default', ['watch:sass']);
+// Rafraîchissement auto des navigateurs web
+gulp.task('browserSynch', function() {
+  browserSynch({
+    server: {
+      baseDir: 'assets'
+    }
+  })
+});
